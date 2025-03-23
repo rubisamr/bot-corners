ef42e5474ecbf13d87619d98c84838ca12b96c56
rubisamr <rubisamr@gmail.com>
2025-03-23 00:51:55 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Instalar Google Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt update
-sudo apt install -y google-chrome-stable
+echo "Instalando Google Chrome..."
+wget -q -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get update
+apt-get install -y ./chrome.deb
+rm chrome.deb
+echo "Google Chrome instalado correctamente."
 
-# Instalar ChromeDriver
+echo "Instalando ChromeDriver..."
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
 DRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
 wget -N "https://chromedriver.storage.googleapis.com/$DRIVER_VERSION/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip
+unzip -o chromedriver_linux64.zip
 chmod +x chromedriver
-sudo mv chromedriver /usr/local/bin/
+mv chromedriver /usr/local/bin/
+rm chromedriver_linux64.zip
+echo "ChromeDriver instalado correctamente."
 
-# Ejecutar el bot
+echo "Ejecutando el bot..."
 python main.py
